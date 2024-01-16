c7a0d45978e332e81bb9da370f364ddc56055a54
Viktor Khasenko <developer@viktorkhasenko.com>
2024-01-16 14:51:58 +0000

    Test and some files were refactored

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -44,31 +44,42 @@ describe('/api/articles/:article_id', () => {
     return request(app)
       .get('/api/articles/1')
       .expect(200)
-      .then((response) => {
-        expect(response.body.article.article_id).toBe(1);
-        expect(response.body.article.title).toBe('Living in the shadow of a great man');
-        expect(response.body.article.author).toBe('butter_bridge');
-        expect(response.body.article.body).toBe('I find this existence challenging');
-        expect(response.body.article.topic).toBe('mitch');
-        expect(response.body.article.created_at).toBe("2020-07-09T20:11:00.000Z");
-        expect(response.body.article.votes).toBe(100);
-        expect(response.body.article.article_img_url).toBe('https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700');
+      .then(({ body }) => {
+        expect(body.article.article_id).toBe(1);
+        expect(body.article.title).toBe('Living in the shadow of a great man');
+        expect(body.article.author).toBe('butter_bridge');
+        expect(body.article.body).toBe('I find this existence challenging');
+        expect(body.article.topic).toBe('mitch');
+        expect(body.article.created_at).toBe("2020-07-09T20:11:00.000Z");
+        expect(body.article.votes).toBe(100);
+        expect(body.article.article_img_url).toBe('https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700');
       });
   });
   test('GET:404 sends an appropriate status and error message when given a valid but non-existent id', () => {
     return request(app)
       .get('/api/articles/999')
       .expect(404)
-      .then((response) => {
-        expect(response.body.msg).toBe('Article does not exist');
+      .then(({ body }) => {
+        expect(body.msg).toBe('Article does not exist');
       });
   });
   test('GET:400 sends an appropriate status and error message when given an invalid id', () => {
     return request(app)
       .get('/api/articles/not-a-article')
       .expect(400)
-      .then((response) => {
-        expect(response.body.msg).toBe('Bad request');
+      .then(({ body }) => {
+        expect(body.msg).toBe('Bad request');
+      });
+  });
+  test("The '/api' endpoint to include a description of this new '/api/articles/:article_id' endpoint.", () => {
+    return request(app)
+      .get("/api")
+      .set("Accept", "application/json")
+      .expect("Content-Type", /json/)
+      .expect(200)
+      .then(({ body }) => {
+        expect(body.endpoints["GET /api/articles/:article_id"]).toBeInstanceOf(Object);
+        expect(body.endpoints["GET /api/articles/:article_id"]).toEqual(endpoints["GET /api/articles/:article_id"]);
       });
   });
 });
